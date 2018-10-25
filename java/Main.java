80ae9ec963c5b8e8bd7350ec3e68324e5068e40f
Abreto Fu <m@abreto.net>
2018-10-25 12:59:46 +0800

    add note for constant and array

--- java/Main.java
+++ java/Main.java
@@ -2,11 +2,15 @@ import java.io.*;
 import java.util.*;  
 import java.math.*;
 
-public class Main { 
-    public static int[] int_array = new int[50050]; /** Array */
+public class Main {
+    public static final int maxn = 50050;
+    public static int[] int_array = new int[maxn]; /** Array */
+    public static int[] another_arr = {1, 2, 3, 5};
     public static void main(String[] args) throws Exception {  
         Scanner cin = new Scanner(System.in); 
         int a = cin.nextInt(), b = cin.nextInt();  
         System.out.println(a + b); 
-    }  
+        for (int i = 0; i < 4; i++)
+            System.out.println(another_arr[i]);
+    }
 }
