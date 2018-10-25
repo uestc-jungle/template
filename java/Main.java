23d3f882264f2adc96c8fa994c360e124766d993
Abreto Fu <m@abreto.net>
2018-10-25 09:57:22 +0800

    add java template

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+import java.io.*; 
+import java.util.*;  
+import java.math.*;
+
+public class Main { 
+    public static int[] int_array = new int[50050]; /** Array */
+    public static void main(String[] args) throws Exception {  
+        Scanner cin = new Scanner(System.in); 
+        int a = cin.nextInt(), b = cin.nextInt();  
+        System.out.println(a + b); 
+    }  
+}
